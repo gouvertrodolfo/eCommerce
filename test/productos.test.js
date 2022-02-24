4facc375cc06ffc3306a1097b691d48faa8f7f87
Rodolfo <rgouvert@gmail.com>
2022-02-24 16:13:41 -0300

    padme

--- test/productos.test.js
+++ test/productos.test.js
@@ -1,5 +1,5 @@
 import app from '../src/server.js'
-import {commerce} from 'faker'
+// import {commerce} from 'faker'
 import supertest from 'supertest'
 import { expect } from 'chai'
 
@@ -19,48 +19,60 @@ describe('Pruebas APIRestFull Productos', () => {
         console.log('\n********* Fin de Test Api Rest*********')
     })
 
+    beforeEach(function () {
+        console.log('\n********* Inicio test unitario*********')
+    })
+
+    afterEach(function () {
+        console.log('\n********* fin test unitario*********')
+    })
+
     describe('Get Listado de productos', () => {
+
         it('debería retornar un status 200', async () => {
             const response = await request.get('/productos/')
             expect(response.status).to.eql(200)
         })
+
     })
 
 
     describe('Post de productos sin persmiso', () => {
-        it('debería retornar un status 401', async () => {
-            const data = {
-                codigo: 'testin',
-                nombre: 'producto de prueba',
-                descripcion: 'producto de prueba generado desde mocha',
-                precio: 14.224,
-                stock: 200
-            };
+        const data = {
+            codigo: 'testin',
+            nombre: 'producto de prueba',
+            descripcion: 'producto de prueba generado desde mocha',
+            precio: 14.224,
+            stock: 200
+        };
 
+        const user = {
+            username: 'r02',
+            password: '123'
+        }
+        const admin = {
+            username: 'admin',
+            password: '123'
+        }
+
+        it('debería retornar un status 401', async () => {
             const response = await request.post('/productos/').send(data)
             expect(response.status).to.eql(401)
         })
-    })
 
-    describe('Post de productos sin persmiso', () => {
+
         it('debería retornar un status 403', async () => {
-            const data = {
-                codigo: 'testin',
-                nombre: 'producto de prueba',
-                descripcion: 'producto de prueba generado desde mocha',
-                precio: 14.224,
-                stock: 200
-            };
-            const user = {
-                "username":"user",
-                "password":"123"
-            }
+
             let response = await request.post('/login').send(user)
             expect(response.status).to.eql(200)
 
-            response = await request.post('/productos').send(data)
-            expect(response.status).to.eql(401)
+            // recupero las cookies del response del login 
+            const cookies = response.headers["set-cookie"].pop().split(";")[0];
+            
+            response =await request.post('/productos').set("Cookie", [cookies]).send(data)
+            expect(response.status).to.eql(403)
         })
+
     })
 
 
